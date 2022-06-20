f196cd00069ec1db64af1d8be8ab9a2537f1ede2
ManuelTano <manueltano98@gmail.com>
2022-06-20 15:36:58 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il 
+// suo colore preferito. Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+// Recupero elemento
+
 const pass = document.getElementById('generator');
+
+// Chiedo il suo nome
 
 const firstname = prompt('Qual è il tuo nome?');
 
+// Chiedo il suo cognome
+
 const lastname = prompt('Qual è il tuo cognome?');
 
+// Chiedo il suo colore preferito
+
 const color = prompt('Qual è il tuo colore preferito?');
+
+// Genero la password con il suo nome, cognome, colore e il numero 21
 
 generator.innerText = (firstname + lastname + color + 21);
